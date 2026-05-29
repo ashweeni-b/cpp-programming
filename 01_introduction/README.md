f71481c84d80cd8c903b0137f592fdae49f4099f
ashweeni-b <bhardeashweeni05@gmail.com>
2026-05-29 21:13:44 +0530

    feat: demonstration of input and output

--- 01_introduction/README.md
+++ 01_introduction/README.md
@@ -7,3 +7,7 @@
 1. Write a program to demonstrate the use of variables and datatypes.
 
     Solution: [Variable and Data Types](./variables.cpp)
+
+1. Write a program to demonstrate the use of input and output
+
+    Solution: [Input and Output](./input_output.cpp)
---
+++ 01_introduction/input_output.cpp
@@ -0,0 +1,22 @@
+/*
+Problem: Demonstration of input and output
+Concept: Input and Output
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int age;
+
+    cout << "Enter age: ";
+
+    // Input age
+    cin >> age;
+
+    // Output age
+    cout << "Age is " << age << endl;
+
+    return 0;
+}
