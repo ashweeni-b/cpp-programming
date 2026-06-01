c1cbd2d2181f1a9083c31ddfe8cd7ba52bf0b814
ashweeni-b <bhardeashweeni05@gmail.com>
2026-06-02 03:00:21 +0530

    feat: HackerRank - Basic Data Types

---
+++ Problems/easy/basic_data_types.cpp
@@ -0,0 +1,27 @@
+/*
+Problem: Basic Data Types
+Concept: Data Types
+Platform: HackerRank
+*/
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char ch;
+    float c;
+    double d;
+    
+    cin >> a >> b >> ch >> c >> d;
+    
+    printf("%d\n", a);
+    printf("%ld\n", b);
+    printf("%c\n", ch);
+    printf("%f\n", c);
+    printf("%lf\n", d);
+    
+    return 0;
+}
