d0154ff42912fbd7e48e73f30db90d98c5b841d5
ashweeni-b <bhardeashweeni05@gmail.com>
2026-06-07 05:15:22 +0530

    feat: add even or odd number program

---
+++ 02_conditional_statements/README.md
@@ -0,0 +1,6 @@
+### Practice Questions
+
+1. Check whether a number is even or odd
+
+    Solution: [Even or Odd](./even_or_odd.cpp)
+
---
+++ 02_conditional_statements/even_or_odd.cpp
@@ -0,0 +1,21 @@
+/*
+Problem: Even or Odd
+Concept: Conditionals and Operators
+*/
+
+#include <iostream>
+
+int main() {
+    int number;
+
+    std::cout << "Enter the number: ";
+    std::cin >> number;
+
+    if(number % 2 == 0) {
+        std::cout << number << " is an even number" << std::endl;
+    } else {
+        std::cout << number << " is an odd number" << std::endl;
+    }
+
+    return 0;
+}
