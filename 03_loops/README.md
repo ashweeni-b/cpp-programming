3148c86a1617a23ee07d95f6cc640b28ec226b66
ashweeni-b <bhardeashweeni05@gmail.com>
2026-06-08 22:07:10 +0530

    feat: add printing 1 to 100 program

---
+++ 03_loops/README.md
@@ -0,0 +1,6 @@
+### Practice Questions
+
+1. Write a program to print numbers from 1 to 100
+
+    Solution: [Print 1 to 100](./print_1_to_100.cpp)
+
---
+++ 03_loops/print_1_to_100.cpp
@@ -0,0 +1,14 @@
+/*
+Problem: Print 1 to 100
+Concept: Loops
+*/
+
+#include <iostream>
+
+int main() {
+    for(int i = 1; i <= 100; i++) {
+        std::cout << i << std::endl;
+    }
+
+    return 0;
+}
